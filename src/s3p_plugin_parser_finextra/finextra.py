0a84b9391b014aa86a5db02b7327f31c5ad93eec
N0kr0s <ka0n5206@gmail.com>
2025-02-03 22:51:11 +0300

    feature(parser): Fix review comments (#6)
    - add profiles
    
    Solves: #6

--- src/s3p_plugin_parser_finextra/finextra.py
+++ src/s3p_plugin_parser_finextra/finextra.py
@@ -1,3 +1,4 @@
+import copy
 import time
 from typing import Iterator
 
@@ -23,103 +24,34 @@ class Finextra(S3PParserBase):
 
         # Тут должны быть инициализированы свойства, характерные для этого парсера. Например: WebDriver
         self.feeds = feeds
-        self.types_additions = ['company', 'channel', 'keyword']
-
-    def find_additions(self, soup, document):
-        # Ищем секцию с дополнительной информацией
-        additional_section = soup.find('div', class_='additional-info')
-        speakers_section = soup.find('div', {'id': 'ctl00_ctl00_body_main_SummaryForm_hSpeakers'})
-        additionals_dict = {}
-        speakers = []
-
-        if additional_section:
-            for addition_name in self.types_additions:
-                # Проверяем, существует ли хотя бы один элемент с классом .info-icon.{addition_name}
-                elements = additional_section.select(f'.info-icon.{addition_name} a')
-                if elements:  # Если список не пуст
-                    additionals_dict[addition_name] = [a.get_text(strip=True) for a in elements]
-                else:
-                    # Если элементы отсутствуют, можно добавить пустой список или другое значение по умолчанию
-                    additionals_dict[addition_name] = []
-            return(additionals_dict)
-        elif speakers_section:
-            for speaker in speakers_section.find_all('li', class_='event-speakers-people-container'):
-                name = speaker.find('h4', class_='event-speakers-people-text-title').text.strip()
-                activity = speaker.find('p').text.strip()
-                speakers.append([name, activity])
-                additionals_dict['speakers'] = speakers
-            return(additionals_dict)
-        else:
-            print(document.link)
-
-    def find_text(self, soup, document):
-        # Ищем блок с текстом статьи
-        article_body = soup.find('div', class_='alt-body-copy')
-        event_summary_article_body = soup.find('div', class_='event-summary alt-body-copy')
-
-        if article_body:
-            article_text = '\n'.join([p.get_text(strip=True) for p in article_body.find_all('p')])
-            return(article_text)
-        elif event_summary_article_body:
-            summary_text = '\n'.join([p.get_text(strip=True) for p in event_summary_article_body.find_all('li')])
-            summary_text += '\n'
-            summary_text += '\n'.join([p.get_text(strip=True) for p in event_summary_article_body.find_all('p')])
-            return (summary_text)
-        else:
-            raise ValueError(f"Article text block not found: {document.link}")
 
     def _parse(self):
         """
-        Функция find_additions ищет доп. информацию на сайте (related company, channels и т.д.).
-        Иногда на сайте нет доп. информации, соответствующей types_additions. На некоторых URL
-        вместо неё могут быть спикеры вебинаров или просто текст (webinar).
-        Поэтому сначала проверяем наличие таких блоков на странице и действуем в зависимости от
-        их наличия (либо ничего не делаем, либо берём спикеров вместо доп. информации).
-
-        Функция find_text ищет блок текста статьи. Если на странице нет блока1(article_body), проверяем наличие
-        блока2(event_summary_article_body). Если есть блок2, сначала извлекаем вопросы из начала статьи, добавляем абзац, а
-        затем берём основной текст статьи.
-
         Парсер сначала получает document из фидов. После запроса страницы передаёт в document.other
         словарь "general", куда добавляется текст, возвращаемый find_text, а затем словарь
         "additionals_dict", возвращаемый find_additions.
         """
-
         if isinstance(self._restriction.maximum_materials, int) and self._restriction.maximum_materials // len(
-                self.feeds) >= 4:
+                self.feeds) >= 2:
             number = self._restriction.maximum_materials // len(self.feeds) + 1
         else:
             number = None
 
         for feed in self.feeds:
             for document in self._slices(
-                    self._rss_feed(
-                        feed
-                    ),
-                    number
+                self._rss_feed(feed),
+                number
             ):
-                 time.sleep(randint(1,3))
-                 # Делаем запрос к странице
-                 response = requests.get(document.link)
-                 if response.status_code != 200:
-                     raise ValueError(f"Failed to access the page. Status code: {response.status_code}")
-
-                 html = response.text
-                 soup = BeautifulSoup(html, 'html.parser')
-
-                 document.other['general'] = {}
-                 document.other['general']['text'] = self.find_text(soup, document)
-                 document.other['general'] = self.find_additions(soup, document)
-
-
-                 try:
-                     self._find(document)
-                 except S3PPluginParserOutOfRestrictionException as e:
-                     self.logger.warning(f"Document {document.id} is outside the specified date range")
-                     if e.restriction == FROM_DATE:
-                         break
-                 except S3PPluginParserFinish as e:
-                     raise e
+                time.sleep(randint(1,3))
+                self._parsed_webpage(document)
+                try:
+                    self._find(document)
+                except S3PPluginParserOutOfRestrictionException as e:
+                    self.logger.warning(f"Document {document.id} is outside the specified date range")
+                    if e.restriction == FROM_DATE:
+                        break
+                except S3PPluginParserFinish as e:
+                    raise e
 
     def _slices(self, feed: Iterator[S3PDocument], number: int | None = None) -> Iterator[S3PDocument]:
         for current, element in enumerate(feed):
@@ -151,3 +83,110 @@ class Finextra(S3PParserBase):
                 parsed_date.replace(tzinfo=None),
                 None,
             )
+
+    def _parsed_webpage(self, document: S3PDocument) -> S3PDocument | None:
+        #Делаем запрос к странице
+        response = requests.get(document.link)
+        if response.status_code != 200:
+            raise ConnectionError(f"Failed to access {self.max_bad_requests} pages. Status code: {response.status_code}")
+
+        html = response.text
+        soup = BeautifulSoup(html, 'html.parser')
+
+        try:
+            return Finextra.ArticlePage(soup, document).document()
+        except Finextra.PageException:
+            ...
+        try:
+            return Finextra.WebinarPage(soup, document).document()
+        except Finextra.PageException:
+            ...
+
+        raise ValueError(f'{document.link} not parsed. Profile is not exist')
+
+    class PageException(Exception):
+
+        def __init__(self, profile, message, errors=None):
+            super().__init__(message)
+            self.errors = errors
+            self.profile = profile
+            self.message = message
+
+        def __repr__(self):
+            return f"Profile {type(self.profile)} Not found"
+
+    class ArticlePage:
+        META: str = 'Article'
+
+        def __init__(self, soup, document: S3PDocument):
+            self.soup = soup
+            self.doc = copy.deepcopy(document)
+            self.doc.other['type'] = self.META
+
+        def document(self) -> S3PDocument:
+            # Main article text
+            article_body = self.soup.find('div', class_='alt-body-copy')
+            if article_body is None:
+                raise Finextra.PageException(self, f'', None)
+            else:
+                article_text = '\n'.join([p.get_text(strip=True) for p in article_body.find_all('p')])
+                self.doc.text = article_text
+
+            # Additional fields
+            types_additions = ['company', 'channel', 'keyword']
+            additional_section = self.soup.find('div', class_='additional-info')
+            additional_dict = {}
+            if additional_section is not None:
+                for addition_name in types_additions:
+                    # Проверяем, существует ли хотя бы один элемент с классом .info-icon.{addition_name}
+                    elements = additional_section.select(f'.info-icon.{addition_name} a')
+                    if elements:  # Если список не пуст
+                        additional_dict[addition_name] = [a.get_text(strip=True) for a in elements]
+                if additional_dict != {}:
+                    self.doc.other['general'] = additional_dict
+
+            return self.doc
+
+    class WebinarPage:
+        META: str = 'Webinar'
+
+        def __init__(self, soup, document: S3PDocument):
+            self.soup = soup
+            self.doc = copy.deepcopy(document)
+            self.doc.other['type'] = self.META
+
+        def document(self) -> S3PDocument:
+            # Main Webinar Text
+            event_summary_article_body = self.soup.find('div', class_='event-summary alt-body-copy')
+            if event_summary_article_body is None:
+                raise Finextra.PageException(self, f'', None)
+            else:
+                thesis = [p.get_text(strip=True) for p in event_summary_article_body.find_all('li')]
+                text = [p.get_text(strip=True) for p in event_summary_article_body.find_all('p')]
+                summary = ''
+                if thesis:
+                    summary += '\n'.join(thesis)
+                if text:
+                    if thesis:
+                        summary += '\n'
+                    summary += '\n'.join(text)
+                self.doc.text = summary
+
+            # Speakers (Optional)
+            if sp := self.speakers():
+                self.doc.other['other'] = sp
+
+            return self.doc
+
+        def speakers(self) -> dict | None:
+            speakers_section = self.soup.find('div', {'id': 'ctl00_ctl00_body_main_SummaryForm_hSpeakers'})
+            additional_dict = {}
+            speakers = []
+            if speakers_section is not None:
+                for speaker in speakers_section.find_all('li', class_='event-speakers-people-container'):
+                    name = speaker.find('h4', class_='event-speakers-people-text-title').text.strip()
+                    activity = speaker.find('p').text.strip()
+                    speakers.append([name, activity])
+                additional_dict['speakers'] = speakers
+                return additional_dict
+            return None
